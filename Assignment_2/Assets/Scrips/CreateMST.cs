a883a4579e6c6a177322eb353b7c39b8b8736822
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2019-02-13 14:25:11 +0100

    Added algorithm for generating minimum spanning tree #1

--- Assignment_2/Assets/Scrips/CreateMST.cs
+++ Assignment_2/Assets/Scrips/CreateMST.cs
@@ -6,6 +6,7 @@ public class CreateMST : MonoBehaviour
 {
     public List<List<GraphNode>> graph;
     public List<GraphNode> cells;
+    public List<List<GraphNode>> mst;
 
     private int gridNoX;
     private int gridNoZ;
@@ -30,7 +31,6 @@ public class CreateMST : MonoBehaviour
             }
         }
 
-
         for (int i = 0; i < gridNoX; ++i)
         {
             for (int j = 0; j < gridNoZ; ++j)
@@ -46,10 +46,11 @@ public class CreateMST : MonoBehaviour
                 }
             }
         }
+
+        mst = Prim();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
     }
@@ -94,9 +95,79 @@ public class CreateMST : MonoBehaviour
         return neighbours;
     }
 
+    public List<List<GraphNode>> Prim()
+    {
+        List<List<GraphNode>> tree = new List<List<GraphNode>>();
+        for (int i = 0; i < cells.Count; ++i)
+        {
+            tree.Add(new List<GraphNode>());
+        }
+
+        List<GraphNode> remaining = new List<GraphNode>();
+        bool[] addedToRemaining = new bool[cells.Count];
+        bool[] inTree = new bool[cells.Count];
+
+        int startNodeIndex;
+        do
+        {
+            startNodeIndex = UnityEngine.Random.Range(0, cells.Count - 1);
+        } while (cells[startNodeIndex].obstacle);
+
+        addedToRemaining[startNodeIndex] = true;
+        inTree[startNodeIndex] = true;
+
+        List<GraphNode> startNeighbours = graph[startNodeIndex];
+        foreach (GraphNode neighbour in startNeighbours)
+        {
+            if (!addedToRemaining[neighbour.i * gridNoZ + neighbour.j])
+            {
+                remaining.Add(neighbour);
+                addedToRemaining[neighbour.i * gridNoZ + neighbour.j] = true;
+            }
+        }
+
+        while (remaining.Count > 0)
+        {
+            //Pick random node to add to the tree
+            int randomIndex = UnityEngine.Random.Range(0, remaining.Count - 1);
+            GraphNode node = remaining[randomIndex];
+            remaining.RemoveAt(randomIndex);
+
+            List<GraphNode> neighbours = graph[node.i * gridNoZ + node.j];
+            bool addedToTree = false;
+            foreach(GraphNode neighbour in neighbours)
+            {
+                //Find node in tree that this node is connected to
+                if(inTree[neighbour.i * gridNoZ + neighbour.j] && !addedToTree)
+                {
+                    //Add this node to the tree
+                    addedToTree = true;
+                    inTree[node.i * gridNoZ + node.j] = true;
+
+                    tree[node.i * gridNoZ + node.j].Add(neighbour);
+                    tree[neighbour.i * gridNoZ + neighbour.j].Add(node);
+                }
+
+                //Add its neighbours to the list
+                if (!addedToRemaining[neighbour.i * gridNoZ + neighbour.j])
+                {
+                    remaining.Add(neighbour);
+                    addedToRemaining[neighbour.i * gridNoZ + neighbour.j] = true;
+                }
+            }
+        }
+
+        return tree;
+    }
+
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.blue;
+        if(!Application.isPlaying)
+        {
+            return;
+        }
+
+        /*Gizmos.color = Color.blue;
         for(int i = 0; i < graph.Count; ++i)
         {
 
@@ -107,6 +178,20 @@ public class CreateMST : MonoBehaviour
             {
                 float nX = terrain_manager.myInfo.get_x_pos(graph[i][j].i);
                 float nZ = terrain_manager.myInfo.get_z_pos(graph[i][j].j);
+                Gizmos.DrawLine(new Vector3(x, 1, z), new Vector3(nX, 1, nZ));
+            }
+        }*/
+        Gizmos.color = Color.white;
+        for (int i = 0; i < mst.Count; ++i)
+        {
+
+            float x = terrain_manager.myInfo.get_x_pos(cells[i].i);
+            float z = terrain_manager.myInfo.get_z_pos(cells[i].j);
+
+            for (int j = 0; j < mst[i].Count; ++j)
+            {
+                float nX = terrain_manager.myInfo.get_x_pos(mst[i][j].i);
+                float nZ = terrain_manager.myInfo.get_z_pos(mst[i][j].j);
                 Gizmos.DrawLine(new Vector3(x, 1, z), new Vector3(nX, 1, nZ));
             }
         }
