25240e53528ed909f80a55540a7b0e1a23b0fdec
Fredrik Omstedt <fredrik.omstedt@gmail.com>
2019-02-16 10:33:54 +0100

    Added simple TSP algorithm #5
    Also updated CarAI script to same as P1, only need small modifications for P2

--- Assignment_2/Assets/Scrips/CarAI2.cs
+++ Assignment_2/Assets/Scrips/CarAI2.cs
@@ -17,11 +17,54 @@ namespace UnityStandardAssets.Vehicles.Car
         public GameObject[] friends;
         public GameObject[] enemies;
 
+        public Dictionary<String, Vector3> initial_positions;
+        public List<Vector3> mstPath;
+        private int currentPathIndex = 0;
+        private float distanceOffset = 5f;
+        private List<Vector3> finalPath;
+
+        public float maxVelocity;
+        public float acceleration;
+        private float timeStep;
+        private int numberOfSteps;
+        private float time;
+
+        private float steerDirection;
+        private float accelerationDirection;
+        private float brake;
+        private float handBrake;
+
+        private bool crashed;
+        private float crashTime;
+        private float crashCheckTime;
+        private float crashDirection;
+        private Vector3 previousPosition;
+        private ConfigurationSpace configurationSpace;
+
         private void Start()
         {
-            // get the car controller
-            m_Car = GetComponent<CarController>();
+            Time.timeScale = 1;
+            maxVelocity = 150;
+            acceleration = 1f;
+
+            timeStep = 0.05f;
+            numberOfSteps = 5;
+            time = 0;
+
+            steerDirection = 0;
+            accelerationDirection = 0;
+            brake = 0;
+            handBrake = 0;
+            crashed = false;
+            crashTime = 0;
+            crashCheckTime = 0.5f;
+            crashDirection = 0;
+            previousPosition = Vector3.up;
+
             terrain_manager = terrain_manager_game_object.GetComponent<TerrainManager>();
+            m_Car = GetComponent<CarController>();
+
+            InitializeCSpace();
 
 
             // note that both arrays will have holes when objects are destroyed
@@ -29,71 +72,236 @@ namespace UnityStandardAssets.Vehicles.Car
             friends = GameObject.FindGameObjectsWithTag("Player");
             enemies = GameObject.FindGameObjectsWithTag("Enemy");
 
+            Debug.Log(string.Format("Number of friends: {0}", friends.Length));
 
 
-            // Plan your path here
-            // ...
+            initial_positions = GetInitPos(friends);
+
+            foreach (var pos in initial_positions)
+            {
+                Debug.Log(pos.ToString());
+            }
+
+            int carNumber = 0;
+            for (int i = 0; i < friends.Length; ++i)
+            {
+                if (friends[i].name == this.name)
+                {
+                    carNumber = i;
+                    break;
+                }
+            }
+
+
+            //TODO: Use VRP path instead
+            Point startPoint = new Point((int)transform.position.x, (int)transform.position.z);
+            Point endPoint = new Point((int)mstPath[0].x, (int)mstPath[0].z);
+
+
+            PathGenerator aStar = new PathGenerator(terrain_manager);
+            List<Vector3> startPath = aStar.GetPath(startPoint, endPoint, transform.rotation.eulerAngles.y);
+            finalPath = startPath;
+
         }
 
+        private Dictionary<String, Vector3> GetInitPos(GameObject[] friends)
+        {
+            Dictionary<String, Vector3> initial_positions = new Dictionary<String, Vector3>();
+
+            for (int i = 0; i < friends.Length; i++)
+            {
+                switch (friends[i].name)
+                {
+                    case "ArmedCar":
+                        initial_positions.Add(friends[i].name,
+                            new Vector3(
+                                terrain_manager.myInfo.get_x_pos(8),
+                                0f,
+                                terrain_manager.myInfo.get_z_pos(9)));
+                        break;
+                    case "ArmedCar (1)":
+                        initial_positions.Add(friends[i].name,
+                            new Vector3(
+                                terrain_manager.myInfo.get_x_pos(8),
+                                0f,
+                                terrain_manager.myInfo.get_z_pos(10)));
+                        break;
+                    case "ArmedCar (2)":
+                        initial_positions.Add(friends[i].name,
+                            new Vector3(
+                                terrain_manager.myInfo.get_x_pos(8),
+                                0f,
+                                terrain_manager.myInfo.get_z_pos(11)));
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return initial_positions;
+        }
 
         private void FixedUpdate()
         {
 
-
-            // Execute your path here
-            // ...
-
-            Vector3 avg_pos = Vector3.zero;
-
-            foreach (GameObject friend in friends)
+            if (Vector3.Distance(finalPath[currentPathIndex], transform.position) <= distanceOffset)
             {
-                avg_pos += friend.transform.position;
-            }
-            avg_pos = avg_pos / friends.Length;
-            Vector3 direction = (avg_pos - transform.position).normalized;
-
-            bool is_to_the_right = Vector3.Dot(direction, transform.right) > 0f;
-            bool is_to_the_front = Vector3.Dot(direction, transform.forward) > 0f;
-
-            float steering = 0f;
-            float acceleration = 0;
-
-            if (is_to_the_right && is_to_the_front)
-            {
-                steering = 1f;
-                acceleration = 1f;
-            }
-            else if (is_to_the_right && !is_to_the_front)
-            {
-                steering = -1f;
-                acceleration = -1f;
-            }
-            else if (!is_to_the_right && is_to_the_front)
-            {
-                steering = -1f;
-                acceleration = 1f;
-            }
-            else if (!is_to_the_right && !is_to_the_front)
-            {
-                steering = 1f;
-                acceleration = -1f;
+                currentPathIndex++;
+                if (currentPathIndex + 3 >= finalPath.Count)
+                {
+                    finalPath = mstPath; //TODO: Use VRP path instead
+                    currentPathIndex = 0;
+                }
             }
 
-            // this is how you access information about the terrain
-            int i = terrain_manager.myInfo.get_i_index(transform.position.x);
-            int j = terrain_manager.myInfo.get_j_index(transform.position.z);
-            float grid_center_x = terrain_manager.myInfo.get_x_pos(i);
-            float grid_center_z = terrain_manager.myInfo.get_z_pos(j);
+            if (!crashed)
+            {
+                time += Time.deltaTime;
+                if (time >= crashCheckTime && Vector3.Distance(m_Car.transform.position, terrain_manager.myInfo.start_pos) > 5)
+                {
+                    time = 0;
+                    if (Vector3.Distance(previousPosition, m_Car.transform.position) < 0.1f)
+                    {
+                        crashed = true;
+                        if (Physics.BoxCast(
+                            m_Car.transform.position,
+                            new Vector3(configurationSpace.BoxSize.x / 2, configurationSpace.BoxSize.y / 2, 0.5f),
+                            m_Car.transform.forward,
+                            Quaternion.LookRotation(m_Car.transform.forward),
+                            configurationSpace.BoxSize.z / 2
+                        ))
+                        {
+                            crashDirection = -1;
+                        }
+                        else
+                        {
+                            crashDirection = 1;
+                        }
 
-            Debug.DrawLine(transform.position, new Vector3(grid_center_x, 0f, grid_center_z));
+                    }
+                    else
+                    {
+                        previousPosition = m_Car.transform.position;
+                    }
+                }
+                steerDirection = SteerInput(m_Car.transform.position, m_Car.transform.eulerAngles.y, finalPath[currentPathIndex]);
+                if (Mathf.Abs(steerDirection) < 0.2f)
+                {
+                    steerDirection = 0;
+                }
+                brake = 0;
+                if (Mathf.Abs(steerDirection) > 0.8f && m_Car.CurrentSpeed > maxVelocity / 10)
+                {
+                    accelerationDirection = 0;
+                    if (m_Car.CurrentSpeed > maxVelocity / 5)
+                    {
+                        handBrake = 1;
+                    }
+                    else
+                    {
+                        handBrake = 0;
+                    }
+                }
+                else
+                {
+                    accelerationDirection = AccelerationInput(m_Car.transform.position, m_Car.transform.eulerAngles.y, finalPath[currentPathIndex]);
+                    handBrake = 0;
+                }
+                if (currentPathIndex < finalPath.Count - 1)
+                {
+                    int stepsToCheck = Mathf.Min(3 + (int)(m_Car.CurrentSpeed * 1.6f * 1.6f * m_Car.CurrentSpeed / 500), finalPath.Count - 1 - currentPathIndex);
+                    for (int i = 1; i <= stepsToCheck; ++i)
+                    {
+                        float steerCheck = SteerInput(m_Car.transform.position, m_Car.transform.eulerAngles.y, finalPath[currentPathIndex + i]);
+                        if (Mathf.Abs(steerCheck) > 0.8f && (m_Car.CurrentSpeed * 1.6f * 1.6f * m_Car.CurrentSpeed) >= Vector3.Distance(m_Car.transform.position, finalPath[currentPathIndex + i]) * 125 * 0.8f)
+                        {
+                            accelerationDirection = 0;
+                            brake = 1;
+                            break;
+                        }
+                    }
+                }
 
+                if (m_Car.CurrentSpeed >= maxVelocity)
+                {
+                    accelerationDirection = 0;
+                }
 
-            // this is how you control the car
-            Debug.Log("Steering:" + steering + " Acceleration:" + acceleration);
-            m_Car.Move(steering, acceleration, acceleration, 0f);
-            //m_Car.Move(0f, -1f, 1f, 0f);
+                if (accelerationDirection < 0)
+                {
+                    m_Car.Move(-steerDirection, brake, accelerationDirection * acceleration, handBrake);
+                }
+                else
+                {
+                    m_Car.Move(steerDirection, accelerationDirection * acceleration, -brake, handBrake);
+                }
+            }
+            else
+            {
+                crashTime += Time.deltaTime;
+                if (crashTime <= 1f)
+                {
+                    steerDirection = SteerInput(m_Car.transform.position, m_Car.transform.eulerAngles.y, finalPath[currentPathIndex]);
+                    if (crashDirection > 0)
+                    {
+                        m_Car.Move(steerDirection, acceleration, 0, 0);
+                    }
+                    else
+                    {
+                        m_Car.Move(-steerDirection, 0, -acceleration, 0);
+                    }
+                }
+                else
+                {
+                    crashTime = 0;
+                    crashed = false;
+                }
+            }
+        }
 
+        //Determines steer angle for the car
+        private float SteerInput(Vector3 position, float theta, Vector3 point)
+        {
+            Vector3 direction = Quaternion.Euler(0, theta, 0) * Vector3.forward;
+            Vector3 directionToPoint = point - position;
+            float angle = Vector3.Angle(direction, directionToPoint) * Mathf.Sign(-direction.x * directionToPoint.z + direction.z * directionToPoint.x);
+            float steerAngle = Mathf.Clamp(angle, -m_Car.m_MaximumSteerAngle, m_Car.m_MaximumSteerAngle) / m_Car.m_MaximumSteerAngle;
+            return steerAngle;
+        }
 
+        //Determines acceleration for the car
+        private float AccelerationInput(Vector3 position, float theta, Vector3 point)
+        {
+            Vector3 direction = Quaternion.Euler(0, theta, 0) * Vector3.forward;
+            Vector3 directionToPoint = point - position;
+            return Mathf.Clamp(direction.x * directionToPoint.x + direction.z * directionToPoint.z, -1, 1);
+        }
+
+        //Get size of car collider to be used with C space
+        private void InitializeCSpace()
+        {
+            Quaternion carRotation = m_Car.transform.rotation;
+            m_Car.transform.rotation = Quaternion.identity;
+            configurationSpace = new ConfigurationSpace();
+            BoxCollider carCollider = GameObject.Find("ColliderBottom").GetComponent<BoxCollider>();
+            configurationSpace.BoxSize = carCollider.transform.TransformVector(carCollider.size);
+            m_Car.transform.rotation = carRotation;
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+            Gizmos.color = Color.red;
+            for (int i = 0; i <= currentPathIndex; ++i)
+            {
+                if (finalPath[i] != null)
+                {
+                    continue;
+                }
+                Gizmos.DrawCube(finalPath[i], Vector3.one);
+            }
         }
     }
 }
---
+++ Assignment_2/Assets/Scrips/TSP.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TSP
+{
+    public static List<Vector3> GreedyPath(List<Vector3> points)
+    {
+        List<Vector3> path = new List<Vector3>() { points[0] };
+        bool[] used = new bool[points.Count];
+        used[0] = true;
+
+        for(int i = 1; i < points.Count; ++i)
+        {
+            Vector3 best = Vector3.one;
+            int bestIndex = 0;
+            for(int j = 1; j < points.Count; ++j)
+            {
+                if(!used[j] && (best == Vector3.one || Vector3.Distance(path[i - 1], points[j]) < Vector3.Distance(path[i - 1], best)))
+                {
+                    best = points[j];
+                    bestIndex = j;
+                }
+            }
+            path[i] = best;
+            used[bestIndex] = true;
+        }
+        return path;
+    }
+}
