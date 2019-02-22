84e5e147a2ed871247d2ca34692e359e68a9292b
Marko Lazic <mlazic@kth.se>
2019-02-22 20:55:28 +0100

    attempt to find a complete solution to p5 #9

--- Assignment_2/Assets/Scrips/CreateGridCost.cs
+++ Assignment_2/Assets/Scrips/CreateGridCost.cs
@@ -24,8 +24,9 @@ public class CreateGridCost: MonoBehaviour
 
         GenerateGrid();
         GenerateCosts();
-        List<SameCostArea> oneTurretAreas = FindAreas(turretRangeCost);
-        path = FindBestPath(startPoint, players[0].transform.rotation.eulerAngles.y, oneTurretAreas);
+
+        this.path = FindPath(startPoint, players[0].transform.rotation.eulerAngles.y);
+
 
     }
 
@@ -44,6 +45,38 @@ public class CreateGridCost: MonoBehaviour
         }
     }
 
+    private void UpdateCostGrid(int indexOfDestroyedTurret)
+    {
+        GameObject turret = enemies[indexOfDestroyedTurret];
+        foreach (CostGridCell cell in grid)
+        {
+            if (cell.isObstacle) continue;
+            float distance = (turret.transform.position - cell.position).magnitude;
+            Vector3 direction = (cell.position - turret.transform.position).normalized;
+            RaycastHit hit;
+            int layer_mask = LayerMask.GetMask("CubeWalls");
+            // Does the ray intersect any objects excluding the player layer
+            if (!Physics.Raycast(turret.transform.position + direction, direction, out hit, distance - 0.5f, layer_mask))
+            {
+                cell.cost /= turretRangeCost;
+            }
+        }
+    }
+
+    private List<Vector3> FindPath(Point startPoint, float angle)
+    {
+        List<Vector3> path = new List<Vector3>();
+        Point point = startPoint;
+        for(int i = 0; i < 4; i++)
+        {
+            List<SameCostArea> oneTurretAreas = FindAreas(turretRangeCost);
+            path.AddRange(FindBestPath(point, 0f, oneTurretAreas));
+            point = new Point((int)path[path.Count - 1].x, (int)path[path.Count - 1].z);
+        }
+
+        return path;
+    }
+
     private List<Vector3> FindBestPath(Point startPoint, float angle, List<SameCostArea> areas)
     {
         List<Vector3> path = new List<Vector3>();
@@ -55,9 +88,11 @@ public class CreateGridCost: MonoBehaviour
 
             int currentPathCost = 0;
             List<Vector3> currentPath = aStar.GetPath(startPoint, new Point((int) area.position.x,(int) area.position.z), angle);
+            /*
             GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
             cube.transform.position = area.position;
             cube.transform.localScale = new Vector3(3, 20, 3);
+            */
             foreach(Vector3 v in currentPath)
             {
                 int i = (int)(v.x - xlow);
@@ -70,6 +105,21 @@ public class CreateGridCost: MonoBehaviour
                 minPathCost = currentPathCost;
                 numberOfCellsInMinPathArea = area.numberOfCells;
                 path = currentPath;
+            }
+        }
+
+
+        for(int i = 0; i < enemies.Length;i++)
+        {
+            if (enemies[i] == null) continue;
+            float distance = (enemies[i].transform.position - path[path.Count - 1]).magnitude;
+            Vector3 direction = (path[path.Count - 1] - enemies[i].transform.position).normalized;
+            RaycastHit hit;
+            int layer_mask = LayerMask.GetMask("CubeWalls");
+            // Does the ray intersect any objects excluding the player layer
+            if (!Physics.Raycast(enemies[i].transform.position + direction, direction, out hit, distance - 0.5f, layer_mask))
+            {
+                UpdateCostGrid(i);
             }
         }
 
